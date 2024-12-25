3d797ed2b9c9c6133ce57b3df0147df9385fa434
Constantin Lisnic <constantin@lisnic.dev>
2024-12-25 09:13:02 +0000

    Initial Commit

---
+++ FlashZilla/FlashZillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashZillaApp.swift
+//  FlashZilla
+//
+//  Created by Constantin Lisnic on 25/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlashZillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlashZillaTests/FlashZillaTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlashZillaTests.swift
+//  FlashZillaTests
+//
+//  Created by Constantin Lisnic on 25/12/2024.
+//
+
+import Testing
+@testable import FlashZilla
+
+struct FlashZillaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
